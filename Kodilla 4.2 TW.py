fcff6c703b1dcd0ebe7feb0e8ddcd263a9325d8c
Tomasz <wrzosek-tomasz@wp.pl>
2021-03-28 21:41:07 +0200

    Commit

--- Kodilla 4.2 TW.py
+++ Kodilla 4.2 TW.py
@@ -1,11 +1,17 @@
 def ispalindrome(word):
     
     word = word.lower()
-    drow = word[::-1]
+    word_no_space = word.replace(" ","")
+    word_no_dot = word_no_space.translate({ord('.'): None})
+    word_no_coma = word_no_dot.translate({ord(','): None})
+    word_no_ask = word_no_coma.translate({ord('?'): None})
+    word_no_interpunkcja = word_no_ask.translate({ord('!'): None})
 
-    if word == drow:
+    drow = word_no_interpunkcja[::-1]
+
+    if word_no_interpunkcja == drow:
         return True
     else:
         return False
  
-print(ispalindrome("ajajajaja"))
+print(ispalindrome("A to kiwi zdziwi kota!!!!"))
